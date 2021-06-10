1ea9c4bd841400549af58ec75e40bcc0e3267a0c
Eduardo Cordeiro <duducordeiro_@hotmail.com>
2021-06-10 13:38:22 -0300

    Testes variados com injeção de dependência

--- src/main/java/com/food/notificacao/NotificadorEmail.java
+++ src/main/java/com/food/notificacao/NotificadorEmail.java
@@ -3,6 +3,8 @@
  */
 package com.food.notificacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +18,21 @@ import com.food.modelo.Cliente;
 // @Primary - faz a desambiguação de componentes que implementam a mesma interface que o Spring tenta instanciar na classe ativação
 //@Qualifier("email") //solução alternativa para desambiguação
 @Profile("prod") //esta annot faz o NotificadorEmail ser registrado somente em produção
-@Component
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+@Component
 public class NotificadorEmail implements Notificador {
 	
-	public NotificadorEmail(){
-		System.out.println("Notificador email real");
-	}
+	@Autowired
+	private NotificadorProperties notificadorProperties;
+	
+	@Value("${notificador.email.porta-servidor}")
+	private Integer porta;
+	
 	
 	@Override
 	public void notificar(Cliente cliente, String msg) {
+		System.out.println("host :" + notificadorProperties.getHostServidor());
+		System.out.println("porta :" + notificadorProperties.getPortaServidor());
 		System.out.printf("Notificando %s através do e-mail %s: %s \n", cliente.getNome(), cliente.getEmail(), msg);
 	}
 
