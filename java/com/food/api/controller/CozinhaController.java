0807ab8adc13e53eb6fd91763238a7f2831ab6c1
Eduardo Cordeiro <duducordeiro_@hotmail.com>
2021-06-13 20:25:38 -0300

    configurações para retornar JSon e XML, inclusão de novos endpoints

--- java/com/food/api/controller/CozinhaController.java
+++ java/com/food/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,15 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	
+	//produces informa o formato que retornará, mesmo a aplicação aceitando outros formatos
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
 	}
 	
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
+	}
 }
